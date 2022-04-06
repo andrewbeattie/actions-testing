478b8ec88a34e3ef737aa62814279758d9f9de8c
Andrew <35040448+andrewbeattie@users.noreply.github.com>
2022-04-06 17:13:10 -0600

    Update manual_settings.py

--- tickspot/net/manual_settings.py
+++ tickspot/net/manual_settings.py
@@ -1,11 +1,6 @@
 from airflow.models import Variable 
 
-class Constants:
-    """
-    URL that are used throughout the script
-    """
 
-    BaseUrl = "https://www.tickspot.com/api/v2/"
-    BaseUrlSubId = "https://www.tickspot.com/%s/api/v2/"
-    UserAgent = {"User-agent": f"DPS ({Variable.get('TICKSPOT_USERNAME')})"}
-    DateFormat = "%Y-%m-%d"
+BaseUrl = "https://www.tickspot.com/api/v2/"
+BaseUrlSubId = "https://www.tickspot.com/%s/api/v2/"
+DateFormat = "%Y-%m-%d"
